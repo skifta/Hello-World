b089bd17834345ecf30b4aa69b9c57091725f1c8
skifta <61063430+skifta@users.noreply.github.com>
2020-02-14 20:36:45 +0100

    Second commit

--- rez.js
+++ rez.js
@@ -1,7 +1,7 @@
 var a = 7;
 var b = "7";
 
-if (a === b) {
-    console.log("ne rregull");
+if (a == b) {
+    console.log("asd");
 } else
-    console.log("gabim");
+    console.log("asddd");
---
+++ skifter.komati.js
@@ -0,0 +1,3 @@
+var Name = prompt("WHat is your name");
+var Surname = prompt("What is your surname");
+console.log("Your full name is " + Name + Surname);
